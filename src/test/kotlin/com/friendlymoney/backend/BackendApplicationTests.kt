c4d6409bec174aa5e5d2d0e15e2e7e4200119f94
Dmitriy <gavrilov.dmitriy04@gmail.com>
2021-03-08 18:23:53 +0300

    initial commit

---
+++ src/test/kotlin/com/friendlymoney/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.friendlymoney.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
